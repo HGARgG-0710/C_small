6b428f76d48335fd8eb27e29f58c6532f7df1d78
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2021-02-17 00:13:42 +0300

    making code in main() in main.c in "power" project  a little bit more structured.

--- power/main.c
+++ power/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "power.h"
 
-int main() {
-    printf("Insert the base of a degree: ");
+int main() { 
     int base;
+    int indicator;
+
+    printf("Insert the base of a degree: ");
     scanf("%d", &base);
 
     printf("Insert the natural indicator of a degree: ");
-    int indicator;
     scanf("%d", &indicator);
 
     printf("Result: %.21g\n\n", power(base, indicator));
