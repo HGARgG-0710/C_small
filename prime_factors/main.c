2f963f33867ec61a671ebda77b9c567a3e516ba0
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2021-05-27 00:27:22 +0300

    Adding time measurment for the prime_factors program.

--- prime_factors/main.c
+++ prime_factors/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+
 #include "../general/general.h"
 
 int main(int argc, char *argv[])
@@ -8,6 +10,8 @@ int main(int argc, char *argv[])
     printf("Please, enter the size of the array, that will be generated with the step 1 and the starting point 0: ");
     scanf("%d", &size);
 
+    clock_t timeStart = clock();
+
     int numbers[size];
     generate(numbers, size, 1, size + 1, 1);
 
@@ -15,6 +19,10 @@ int main(int argc, char *argv[])
     int primes[primes_len];
     get_primes(primes, numbers, size);
 
+    clock_t timeEnd = clock();
+
     for (int i = 0; i < primes_len; i++)
         printf("%d: %d\n", i, primes[i]);
+
+    printf("Took milliseconds to run: %.20f", ((double)(timeEnd - timeStart)) / (CLOCKS_PER_SEC / 1000));
 }
